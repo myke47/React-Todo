b3250d7e6b2d2e363e272ef22f5d4e50b7cd1d80
Myke47 <mychel.larson@gmail.com>
2019-05-14 18:35:24 -0700

    working through architecture

--- src/components/TodoComponents/TodoList.js
+++ src/components/TodoComponents/TodoList.js
@@ -1,2 +1,20 @@
+import React from 'react';
+// import Todo from './Todo';
+// import './components/TodoComponents/Todo.css';
+
 // your components will all go in this `component` directory.
 // feel free to change this component.js into TodoList.js
+
+//         <Todo />
+// Todo component needs to be worked before refactoring this list. 
+const TodoList = props => {
+    render (
+        <div>
+        <h1>This is the first Todo!</h1>
+
+
+        </div>
+    );
+};
+
+export default TodoList;
