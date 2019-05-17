b917e19b0236d5d7210649c71c019dbd2bbbca14
Myke47 <mychel.larson@gmail.com>
2019-05-16 21:18:18 -0700

    inor style update to tasks

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+// import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
 import TodoForm from "./components/TodoComponents/TodoForm";
 import TodoList from "./components/TodoComponents/TodoList";
 import "./components/TodoComponents/Todo.css";
